2392e02860805123879ac414134b2c6fa754ae17
Fabio Tunno <fat@fatshotty.net>
2018-12-12 16:50:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Events
+Simple events management for all object/HTML elements
+
+##### Usage
+Events exposes static methods
+```js
+import Events from '@openmind/om-events';
+```
+
+### on / one
+Attach events to `target` object
+```js
+Events.on( target, 'eventname', function callback() {});
+Events.one( target, 'eventname', function callback() {});
+```
+You can also use attach more than one event per time. You can use namespaces, too
+```js
+Events.on( target, 'eventname.namespace', function callback() {});
+Events.on( target, 'eventname.namespace.subspace', function callback() {});
+Events.on( target, 'eventname.namespace otherevent.namespace', function callback() {});
+```
+
+### off
+Detach event from `target` object
+```js
+Events.off( target, 'eventname'); // detach all event-callbacks associated to given `eventname`
+Events.off( target, 'eventname', callback); // detach given event-callback to given `eventname`
+Events.off( target, 'eventname.namespace'); detach all event-callback associated to given `eventname` with given `namespace`
+Events.off( target, '.namespace'); detach all events-callbacks with given 'namespace'
+```
+
+### trigger
+Trigger event to `target` object
+```js
+Events.trigger( target, 'eventname'); // detach all event-callbacks associated to given `eventname`
+Events.trigger( target, 'eventname', callback); // detach given event-callback to given `eventname`
+Events.trigger( target, 'eventname.namespace'); detach all event-callback associated to given `eventname` with given `namespace`
+Events.trigger( target, '.namespace'); detach all events-callbacks with given 'namespace'
+```
+
+### Dynamic methods
+You can use dynamic methods
+```js
+const eventObject = new Events(target);
+```
+### Standard methods
+Use all previous methods such as
+```js
+eventObject.on('eventname', function callback() {});
+```
---
+++ src/events.js
@@ -0,0 +1,189 @@
+const extractHandlers = (collection, eventnames) => {
+
+  const getHandlersFromNamespace = (coll) => {
+
+    let ret1 = [ coll.handlers ];
+    for( const section in coll.subevents ) {
+      if ( coll.subevents.hasOwnProperty(section) ) {
+        ret1 = ret1.concat( getHandlersFromNamespace( coll.subevents[section] ) );
+      }
+    }
+    return ret1;
+  };
+
+  let ret = [];
+
+  eventnames = eventnames.split('.');
+  const eventname = eventnames.shift();
+  if ( eventname === '' ) {
+    for( const section in collection.subevents ) {
+      if ( collection.subevents.hasOwnProperty(section) ) {
+        ret = ret.concat(  extractHandlers( collection.subevents[ section ], eventnames.join('.') )  )
+      }
+    }
+
+  } else {
+    const section = eventname;
+    if ( collection.subevents.hasOwnProperty(section) ) {
+
+      ret = ret.concat( getHandlersFromNamespace( collection.subevents[section] ) );
+    }
+  }
+
+  return ret;
+
+};
+
+
+const checkFn = (fn, callback) => {
+  return (fn === callback) || (fn.__Ref__ === callback) || (fn === callback.__Ref__) || (fn.__Ref__ === callback.__Ref__);
+}
+
+class Events {
+
+  static get VERSION() {
+    return process.env.VERSION;
+  }
+
+  constructor(wrapper) {
+    this.__event_target__ = wrapper;
+  }
+
+  on(name, callback, ...args) {
+    Events.on(this.__event_target__, name, callback, args);
+    return this;
+  }
+
+  one(name, callback, args) {
+    Events.one(this.__event_target__, name, callback, args);
+    return this;
+  }
+
+  off(name, callback) {
+    Events.off(this.__event_target__, name, callback);
+    return this;
+  }
+
+  trigger(name, ...args) {
+    Events.trigger(this.__event_target__, name, args);
+    return this;
+  }
+
+  static get DefaultObject() {
+    return {'handlers': [], 'subevents': {}};
+  }
+
+  static on(target, name, callback, ...args) {
+
+    let __Events__ = target.__Events__ = target.__Events__ || Events.DefaultObject;
+
+    const evt = name.split(' ');
+    for( let e of evt ) {
+      e = e.trim();
+      const es = e.split('.');
+      let index = 0;
+      while( index < es.length - 1 ) {
+        const key = es[ index++ ];
+        if ( ! key ) {
+          throw `invalid event name ${e}`;
+        }
+        __Events__.subevents[ key ] = __Events__.subevents[ key ] || Events.DefaultObject;
+        __Events__ = __Events__.subevents[ key ];
+      }
+
+
+      let event_object = __Events__.subevents[ es[ index ] ];
+      if ( !event_object ){
+        event_object = __Events__.subevents[ es[ index ] ] = Events.DefaultObject;
+      }
+
+      const handlers = event_object.handlers;
+
+      callback.__Ref__ = function() {
+        let _arg = Array.prototype.slice.call( arguments, 0 );
+        return callback.apply( target, _arg.concat( args ) );
+      };
+      handlers.push( callback );
+
+      if ( target.addEventListener ) {
+        target.addEventListener( es[0], callback.__Ref__, false );
+      }
+    }
+
+  }
+
+
+  static one(target, name, callback, ...args) {
+
+    const tmp = function() {
+      const ret = callback.apply(target, arguments);
+      Events.off( target, name, tmp);
+      return ret;
+    };
+
+    Events.on(target, name, tmp, args);
+  }
+
+
+  static off(target, name, callback) {
+
+    target.__Events__ = target.__Events__ || Events.DefaultObject;
+
+    name = name || '.';
+
+    let events_to_remove = [];
+
+    if ( target.removeEventListener ) {
+      const names_split = name.split('.');
+      if ( names_split[0] === '' ) {
+        events_to_remove = Object.keys(target.__Events__.subevents);
+      } else {
+        events_to_remove = [ names_split[0] ];
+      }
+    }
+
+    const handlers = extractHandlers( target.__Events__, name );
+
+    for( let i = handlers.length - 1; i >= 0; i-- ) {
+      const pos = handlers[ i ];
+      for( let j = pos.length - 1; j >= 0; j-- ){
+        const fn = pos[ j ];
+        if ( callback ) {
+          if ( checkFn(fn, callback) ){
+            pos.splice(j, 1);
+            for( const k of events_to_remove ) {
+              target.removeEventListener( k, fn.__Ref__ || fn, false );
+            }
+          }
+        } else {
+          pos.splice(j, 1);
+          for( const k of events_to_remove ) {
+            target.removeEventListener( k, fn.__Ref__ || fn, false );
+          }
+        }
+      }
+    }
+
+  }
+
+  static trigger(target, name, ...args) {
+
+    target.__Events__ = target.__Events__ || Events.DefaultObject;
+
+    const eventnames = name.split('.')
+    const handlers = extractHandlers( target.__Events__, name );
+
+    for( const hs of handlers ) {
+      for( const fn of hs ) {
+        fn.apply(target, args);
+      }
+    }
+    if ( target[ eventnames[0] ] ) {
+      // trigger native events
+      target[ eventnames[0] ]();
+    }
+  }
+
+}
+
+export default Events;
